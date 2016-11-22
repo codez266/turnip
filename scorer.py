4f93394f23413c495895b4db63f92821b34b5414
iitp <iitp@gitlab.cse.iitp.sc.in>
2016-11-22 17:52:10 +0530

    Add fetching wikipedia articles in scorer

--- scorer.py
+++ scorer.py
@@ -1,3 +1,13 @@
+import re
+import logging
+import mwparserfromhell as mwparser
+import mwapi
+import os
+import urllib
+import math
+from nltk import PorterStemmer
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 class Scorer(object):
     def __init__(self, topics, pairs, mode):
         self.topics = topics
@@ -62,6 +72,54 @@ class Scorer(object):
         return persons
 
     @staticmethod
+    def getWikipediaTexts2(persons):
+        session = mwapi.Session("https://en.wikipedia.org", user_agent = "test" )
+        for p in persons:
+            if not os.path.isfile('articles/'+p):
+                try:
+                    logger.info("Requesting %s", p)
+                    page = Scorer.fetchArticle(p, session)
+                    persons[p]['text'] = page[p]
+                    f = open('articles/'+p, 'w')
+                    logger.info("Writing back: %s", p)
+                    f.write(page[p])
+                    f.close()
+                except Exception as e:
+                    print("Failed for:"+p)
+                    logger.error("Falie for: %s\n %s", p, str(e))
+            else:
+                logger.info("Reading local: %s", p)
+                f = open('articles/'+p)
+                persons[p]['text'] = f.read()
+                f.close()
+        return persons
+    
+    @staticmethod
+    def fetchArticle(title, session):
+        pagecontent = {}
+        doc = session.get(action="query", prop="revisions", rvprop="content", titles=title)
+        pages = doc["query"]["pages"]
+        for key, page in pages.items():
+            text = page["revisions"][0]["*"]
+            pagecontent[page["title"]] = Scorer.process_sentence(str(mwparser.parse(text)))
+        return pagecontent
+
+    @staticmethod
+    def repl(m):
+        return ''
+    
+    @staticmethod
+    def process_sentence(sentence):
+        # a heavenly regex, replaces mentions with actual names!
+        t = re.sub(r"\[(.*?)\|(.*?)\]",r"\1",sentence)
+        t = re.sub(r"[\(\)\.\[\]%<>',\":;0-9]", " ", t)
+        t = re.sub(r"http.*", "",t)
+        t = re.sub(r"\-", " ", t)
+        t = re.sub(r"[^a-zA-Z_ \n]", "", t, flags=re.UNICODE)
+        t = t.strip()
+        return t
+
+    @staticmethod
     def maplog(list):
         max = -1
         mapped = []
@@ -70,7 +128,7 @@ class Scorer(object):
                 max = s
         for i, s in enumerate(list):
             if max != 0:
-                if s > 0:
+                if s > 1:
                     mapped.append(s / max)
                     mapped[i] = mapped[i] * (2**7)
                     mapped[i] = int(math.log(mapped[i], 2))
@@ -107,7 +165,8 @@ class CountScorer(Scorer):
         topicwords = Scorer.getTopicData('data/indicators-f')
 
     def score(self):
-        self.persons = Scorer.getWikipediaTexts('../persons3', self.persons)
+        stemmer = PorterStemmer()
+        self.persons = Scorer.getWikipediaTexts2(self.persons)
         dummy = ""
         topicwords = self.getTopicData('data/indicators-f')
         i = 0
@@ -121,30 +180,47 @@ class CountScorer(Scorer):
                 for p in proflist:
                     #print(p)
                     if p in topicwords:
-                        words = [x.lower() for x in topicwords[p]]
+                        words = [stemmer.stem(x.lower()) for x in topicwords[p]]
                         #print(words)
                         #print("calculating for:", per, p)
                         text = self.persons[per]['text']
                         score = 0
                         for w in text.split():
-                            if w in words:
+                            if stemmer.stem(w.lower()) in words:
                                 score = score + 1
                         #print(score)
                         scorelist.append(score)
                     else:
                         scorelist.append(0)
                 # all professions scored
-                scorelist = Scorer.maplin(scorelist)
+                scorelist = Scorer.maplog(scorelist)
                 # assign the scores to persons pair
                 for j in range(i, i + len(scorelist)):
                     #print("Scoring %s, %s:%d",self.pairs[j][0], self.pairs[j][1],scorelist[j-i])
                     self.pairs[j].append(scorelist[j-i])
             dummy = per
             i = i + 1
-        self.writeScore('data/profession.out')
 
-topics = Scorer.getTopics('data/professions')
-pairs = Scorer.getPersonPairs('data/profession.train')
-sc = CountScorer(topics, pairs, 'profession')
-sc.score()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help="Input directory", action="store_true")
+    parser.add_argument("input", help="Input directory value")
 
+    parser.add_argument("-o", help="Output directory", action="store_true")
+    parser.add_argument("output", help="Output directory value")
+    args = parser.parse_args()
+
+    input = ""
+    output = ""
+    if args.i:
+        input = args.input
+    if args.o:
+        output = args.output
+
+    topics = Scorer.getTopics(input+'/professions')
+    pairs = Scorer.getPersonPairs(input+'/profession.train')
+    sc = CountScorer(topics, pairs, 'profession')
+    sc.score()
+    sc.writeScore(output+'/profession.out')
+if __name__ == '__main__':
+    main()
