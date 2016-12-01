609e299f11ecc5b6b473e1cc6415e9a6499d2f81
iitp <iitp@gitlab.cse.iitp.sc.in>
2016-12-01 19:11:12 +0530

    Modify wordtovec to online

--- word2vec.py
+++ word2vec.py
@@ -5,35 +5,18 @@ import os.path
 import sys
 import multiprocessing
 from gensim.models import Word2Vec
-from gensim.models.word2vec import LineSentence
-from gensim.parsing.preprocessing import STOPWORDS
-from gensim.parsing import PorterStemmer
 import string
 
-
-def processToken(token, stemmer):
-    if token not in STOPWORDS and token not in string.punctuation:
-        return stemmer.stem(token.lower())
-    else:
-        return ""
-
-
-def preprocess(file, logger):
-    logger.info("starting preprocess...")
-    stemmer = PorterStemmer()
-    f = open(file)
-    lines = []
-    for line in f:
-        str = ""
-        for word in line.split():
-            w = processToken(word, stemmer)
-            if w != "":
-                str = str + w + " "
-        lines.append(str.strip())
-    f.close()
-    logger.info("ending preprocess...")
-    return lines
-
+class MySentences(object):
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def __iter__(self):
+        for line in open(self.filename, 'r'):
+            textline = line.lower()
+            text=re.sub(r'\[(.*?)\|(.*?)\]',r'\1',textline)
+            text=re.sub(r'[\[\]\(\)\:"\.;\',]',r'', text)
+            yield text.split()
 
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
@@ -48,7 +31,7 @@ if __name__ == '__main__':
         sys.exit(1)
     inp, outp = sys.argv[1:3]
 
-    model = Word2Vec(LineSentence(inp), size = 400, window = 5, min_count = 5,
+    model = Word2Vec(MySentences(inp), size = 400, window = 8, min_count = 5,
                      workers = multiprocessing.cpu_count())
 
     model.init_sims(replace=True)
